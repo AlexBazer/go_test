1e5089a3d1575220a6cdd31fcca344c242138a8f
Alexandr Balagura <davnobezimeni@gmail.com>
2016-04-14 05:53:50 +0300

    Pass tests

--- common_child/main.go
+++ common_child/main.go
@@ -5,48 +5,44 @@ import "fmt"
 // https://www.hackerrank.com/challenges/common-child
 
 func main() {
-    var str1, str2 string
+	var str1, str2 string
 
-    fmt.Scan(&str1, &str2)
-    fmt.Println(findMaxSeqLen(str1, str2))
+	fmt.Scan(&str1, &str2)
+	// Create store 2d array to sote recursion values to gain speed
+	store := make([][]int, len(str1))
+	for i := 0; i < len(str1); i++ {
+		store[i] = make([]int, len(str2))
+		for j := 0; j < len(str2); j++ {
+			store[i][j] = -1
+		}
+	}
+	fmt.Println(findMaxSeqLen(str1, str2, len(str1)-1, len(str2)-1, store))
 }
 
-func findMaxLen(str1, str2 string)int{
-    i := 0
-    j := 0
-    var max int
+func findMaxSeqLen(str1, str2 string, i, j int, store [][]int) int {
+	if i+1 == 0 || j+1 == 0 {
+		return 0
+	}
+	if store[i][j] >= 0 {
+		return store[i][j]
+	}
 
-    for i<str{
-        for j>0{
-            if str1[i] == str2[j]{
-                max ++
-                i--
-                j--
-            }
-
-        }
-    }
-
-    return max
+	var storeVal int
+	if str1[i] == str2[j] {
+		storeVal = 1 + findMaxSeqLen(str1, str2, i-1, j-1, store)
+	} else {
+		storeVal = maxArg(findMaxSeqLen(str1, str2, i, j-1, store), findMaxSeqLen(str1, str2, i-1, j, store))
+	}
+	store[i][j] = storeVal
+	return storeVal
 }
 
-func findMaxSeqLen(str1, str2 string) int{
-    if len(str1) == 0 || len(str2) == 0 {
-        return 0
-    }
-
-    if str1[len(str1)-1] == str2[len(str2)-1]{
-        return 1 + findMaxSeqLen(str1[:len(str1)-1], str2[:len(str2)-1])
-    }
-    return maxArg(findMaxSeqLen(str1, str2[:len(str2)-1]), findMaxSeqLen(str1[:len(str1)-1], str2))
-}
-
-func maxArg(args ...int) int{
-    var maxVal int
-    for i:=0;i<len(args);i++{
-        if args[i]>maxVal{
-            maxVal = args[i]
-        }
-    }
-    return maxVal
+func maxArg(args ...int) int {
+	var maxVal int
+	for i := 0; i < len(args); i++ {
+		if args[i] > maxVal {
+			maxVal = args[i]
+		}
+	}
+	return maxVal
 }
